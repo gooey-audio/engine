339939e9857ae3e6055b71df5e863378a5122d6b
Brian Hurlow <brian@brianhurlow.com>
2025-07-22 08:55:12 -0400

    updates

--- debug-ui/src/kick.ts
+++ debug-ui/src/kick.ts
@@ -1,12 +1,12 @@
 import { Instrument } from "./instrument";
 import { Oscillator } from "./oscillator";
 
-export const makeKick = (ctx: AudioContext) => {
+export const makeKick = (ctx: AudioContext, freq1: number, freq2: number) => {
   const inst = new Instrument(ctx);
 
-  const osc1 = new Oscillator(ctx, 500);
+  const osc1 = new Oscillator(ctx, freq1);
 
-  const osc2 = new Oscillator(ctx, 800);
+  const osc2 = new Oscillator(ctx, freq2);
 
   inst.addOsc("sub", osc1);
   inst.addOsc("main", osc2);
